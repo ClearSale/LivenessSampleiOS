afa9f652ab110b8eb58bc6eaa3964812007041cd
lucas.carvalho <lucas.carvalho@MACDEV007988.local>
2023-01-18 13:47:15 -0300

    Completed project

--- LivenessSampleiOS/ViewController.swift
+++ LivenessSampleiOS/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import CSLivenessSDK
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnCallSDK: UIButton!
+    private var livenessSdk: CSLiveness?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func didTapButton(_ sender: Any) {
+        force(portrait: true)
+        DispatchQueue.main.async {
+            self.livenessSdk = CSLiveness(
+                configurations: CSLivenessConfigurations(
+                    clientId: "SEU CLIENT ID AQUI",
+                    clientSecret: "SEU SECRET ID AQUI"
+                )
+            )
+            
+            self.livenessSdk?.delegate = self
+            self.livenessSdk?.start(viewController: self, animated: true)
+        }
+    }
+}
+
+
+// MARK: - Clearsale liveness Delegate
+
+extension ViewController: CSLivenessDelegate {
+    func liveness(didOpen: Bool) {
+        print(didOpen)
+    }
+    
+    func liveness(error: CSLivenessError) {
+        print(error)
+        print("*********** \(error.rawValue) ***********")
+        force(portrait: false)
+    }
+    
+    func liveness(success: CSLivenessResult) {
+        print(success)
+        print("*********** \(success) ***********")
+        force(portrait: false)
     }
 
-
+    func force(portrait: Bool)  {
+        DispatchQueue.main.async {
+            UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue,forKey: "orientation")
+        }
+    }
 }
 
