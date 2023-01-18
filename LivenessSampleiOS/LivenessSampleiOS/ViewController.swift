216ffc1d696f5f03cb664228e71cdefb8c3656ff
lucas.carvalho <lucas.carvalho@MACDEV007988.local>
2023-01-18 12:45:58 -0300

    Base project

---
+++ LivenessSampleiOS/LivenessSampleiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LivenessSampleiOS
+//
+//  Created by lucas.carvalho on 18/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
